942f084aeca281a8cce27afe13f9926ff0382621
Rohit Kumar Shrivastava <rohit3345kumar@gmail.com>
2025-12-17 10:49:34 +0530

    feat: added support for share url, validation and cursor

--- script.js
+++ script.js
@@ -2,27 +2,31 @@
  * This method extracts video id from a youtube video URL
  * @param {string} url: youtube video URL
  */
+
 function getVideoIdFromURL(url) {
-    if (!url) return "";
-    if (!url.startsWith("http")) return "";
-    console.log(url, "url rohit");
-    const urlObj = new URL(url);
-    console.log(urlObj, "urlObj rohit");
-    const urlSearchParams = new URLSearchParams(urlObj.searchParams);
-    console.log(urlSearchParams, "url search params rohit");
-    return urlSearchParams.get("v");
-}
-
-function showYouTubeVideo() {
-    const inputValue = document.querySelector("#youtube-video-id").value;
-    const videoId = getVideoIdFromURL(inputValue);
-    console.log(videoId, "videoId rohit");
-    const iframeContainer = document.querySelector("#youtube-iframe");
-    iframeContainer.setAttribute("src", `https://www.youtube.com/embed/${videoId}`);
-}
-
-function handleKeyUp(event) {
-    if (event.key === "Enter") {
-        showYouTubeVideo();
+    try {
+        if (!url) return "";
+        if (!url.startsWith("http")) return "";
+        let videoId = "";
+        const urlObj = new URL(url);
+        const urlSearchParams = new URLSearchParams(urlObj.searchParams);
+        if (urlObj.host === "youtu.be") {
+            videoId = urlObj.pathname;
+        } else {
+            videoId = urlSearchParams.get("v");
+        }
+        return [videoId, urlSearchParams.get("t")?.replace("s", "") ?? "0"];
     }
+    catch (error) {
+        console.log(error);
+        return ["", "0"];
+    }
+}
+
+function showYouTubeVideo(event) {
+    event.preventDefault();
+    const inputValue = document.querySelector("#youtube-video-id").value;
+    const [videoId, start] = getVideoIdFromURL(inputValue);
+    const iframeContainer = document.querySelector("#youtube-iframe");
+    iframeContainer.setAttribute("src", `https://www.youtube.com/embed/${videoId}?si=zMRERwwT9CMmSE7s&amp;start=${start}`);
 }
